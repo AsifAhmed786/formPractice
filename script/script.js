e3c1721f456003795a9f96d491fee1f35aa830e3
asifahmed786 <asifahmed1us1@gmail.com>
2020-05-09 22:59:23 +0500

    practice day 2nd

---
+++ script/script.js
@@ -0,0 +1,16 @@
+// var a = 20;
+// var b = "20";
+// var c = a*b;
+// alert(c);
+// alert(a==b);
+// var input = prompt("Please enter your name")
+// alert(input)
+
+
+// // console.log(a==b)
+
+var a = 1;
+var b = a++ + ++a + --a + a++ + --a;
+        1      3     2     2     2
+alert(b);
+alert(a);
